9bb713852e0012532b3ab1197793c98c6f282819
unknown <412588801@qq.com>
2024-05-20 16:59:25 +0800

    global

--- JsGui/JsEnv.cpp
+++ JsGui/JsEnv.cpp
@@ -5,24 +5,12 @@
 #include <filesystem>
 #include <fstream>
 #include "Win.h"
-#include "Util.h"
+#include "Global.h"
 
 
 static JsEnv* env;
 JSContext* ctx;
 JSRuntime* rt;
-
-static JSValue js_console_log(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst* argv) {
-    for (int i = 0; i < argc; i++) {
-        const char* str = JS_ToCString(ctx, argv[i]);
-        if (str) {
-            printf("%s", str);
-            JS_FreeCString(ctx, str);
-        }
-    }
-    printf("\n");
-    return MakeVal(0, JS_TAG_UNDEFINED);
-}
 
 static JSContext* JS_NewCustomContext(JSRuntime* rt)
 {
@@ -50,17 +38,7 @@ JsEnv::JsEnv()
         return;
     }
 
-    js_init_module_std(ctx, "std");
-    js_std_add_helpers(ctx, 0, nullptr);
-    js_init_module_os(ctx, "os");
-
-
-    JSValue global_obj = JS_GetGlobalObject(ctx);
-    JSValue consoleObj = JS_NewObject(ctx);
-    JS_SetPropertyStr(ctx, consoleObj, "log", JS_NewCFunction(ctx, js_console_log, "log", 1));
-    JS_SetPropertyStr(ctx, global_obj, "console", consoleObj);
-    JS_FreeValue(ctx, global_obj);
-
+    Global::Reg(ctx);
     Win::Reg(ctx);
     LoadIndexJs(ctx);
     js_std_loop(ctx);
