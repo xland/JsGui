40d9bf96aeaa9d6f4e5f03882da51dac4804c7e7
unknown <412588801@qq.com>
2024-05-20 10:26:32 +0800

    Update main.cpp

--- JsGui/main.cpp
+++ JsGui/main.cpp
@@ -21,14 +21,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
     
     auto winId = webui_new_window();
     //webui_set_position(winId, 300, 300);
-    webui_show_browser(winId, "<html><script src=\"webui.js\"></script> Hello World from C++! <button id=\"MyID\">Hello</button> </html>", webui_browsers::Edge);
+    webui_show(winId, "<html><script src=\"webui.js\"></script> Hello World from C++! <button id=\"MyID\">Hello</button> </html>");
     //webui_run(winId, "alert('Fast!');");
     webui_bind(winId, "MyID", my_function);
-
-
-    webui_set_timeout(3000);
     webui_wait();
-
 
 
     //JsEnv::Init();    
